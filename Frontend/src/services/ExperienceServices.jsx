ecc6537b37e98e6764ce3d6ceadc9c6c9a266875
GOGRAJ DADARWAL <ramkunwarjat00@gmail.com>
2025-09-05 12:41:34 +0530

    Update ExperienceServices.jsx  base URL

--- Frontend/src/services/ExperienceServices.jsx
+++ Frontend/src/services/ExperienceServices.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const experienceApi = axios.create({
-    baseURL: "http://localhost:3000/api/v1/experience"
+    baseURL: "http://preduskbackend.vercel.app/api/v1/experience"
 });
 
 
