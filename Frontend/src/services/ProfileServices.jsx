8dfc6021bc4a07ddc64dedc8c077f91232ad6cb0
GOGRAJ DADARWAL <ramkunwarjat00@gmail.com>
2025-09-05 14:54:35 +0530

    Update ProfileServices.jsx

--- Frontend/src/services/ProfileServices.jsx
+++ Frontend/src/services/ProfileServices.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const profileApi = axios.create({
-    baseURL: "http://preduskbackend.vercel.app/api/v1/profile"
+    baseURL: "https://preduskbackend-ouvvu466c-gograj-choudharys-projects.vercel.app/api/v1/profile"
 });
 
 
