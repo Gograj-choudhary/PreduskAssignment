ca2f2213a5b8d76ce20e51835a5420445c5f571d
GOGRAJ DADARWAL <ramkunwarjat00@gmail.com>
2025-09-05 12:42:51 +0530

    Update SkillServices.jsx base URL

--- Frontend/src/services/SkillServices.jsx
+++ Frontend/src/services/SkillServices.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const skillApi = axios.create({
-    baseURL: "http://localhost:3000/api/v1/skill"
+    baseURL: "http://preduskbackend.vercel.app/api/v1/skill"
 });
 
 
